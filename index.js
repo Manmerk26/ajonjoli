692206834e25be0e60165a10e07654f1e1112eee
juan mercado <juanmerk35@gmail.com>
2022-08-28 12:10:07 -0300

    AjonjoliJavascript+Mercado

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+let entrada = parseFloat( prompt("Ingrese el producto"));
+while (entrada != 'ESC'){
+switch(entrada){
+case "babyshowers":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "cumpleaños":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "comuniones":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "props":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "bautismos":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "books":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "gigantografias":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    case "invitaciones digitales":
+    alert(parseInt(prompt("Ingrese su contacto y nos comunicaremos a la brevedad")));
+    break;
+    default:
+        alert(Error);
+        break;
+}
+entrada = prompt("Ingrese el producto");
+}
